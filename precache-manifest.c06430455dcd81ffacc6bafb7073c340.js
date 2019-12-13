8979cf40614cf98ec13a5e32c3443a8658c13b3a
David Hersey <david.hersey@holidayextras.com>
2019-12-13 12:51:35 +0000

    Updates

---
+++ precache-manifest.c06430455dcd81ffacc6bafb7073c340.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3056dfefdf83295efa1641c4a3827c92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45871a546a5a12f8aa13",
+    "url": "/static/css/main.5c5df2d8.chunk.css"
+  },
+  {
+    "revision": "9d3c7972e87dfa244e7e",
+    "url": "/static/js/2.115580e1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.115580e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7ac478ad3ce83d661871",
+    "url": "/static/js/3.4b37f1ec.chunk.js"
+  },
+  {
+    "revision": "d7c487f6a4e0f6660ccc",
+    "url": "/static/js/4.93d5cf90.chunk.js"
+  },
+  {
+    "revision": "e518cf973583a385cb39",
+    "url": "/static/js/5.d4472c87.chunk.js"
+  },
+  {
+    "revision": "9c144cf3b5d6a944a5e7",
+    "url": "/static/js/6.422591a4.chunk.js"
+  },
+  {
+    "revision": "0c2447824e960136c3ad",
+    "url": "/static/js/7.a343f4f8.chunk.js"
+  },
+  {
+    "revision": "45871a546a5a12f8aa13",
+    "url": "/static/js/main.81b8873b.chunk.js"
+  },
+  {
+    "revision": "3fed120b92bf06a432ea",
+    "url": "/static/js/runtime-main.81e7b649.js"
+  }
+]);
