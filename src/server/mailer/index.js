4813732610ed93da9c78c9b48fe2a4ad0e5806cd
David Hersey <david.hersey@holidayextras.com>
2020-05-01 15:18:26 +0100

    Testing of forms

---
+++ src/server/mailer/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+const nodemailer = require('nodemailer')
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
+  }
+});
+
+const send = ({ email, name, text }) => {
+  const from = name && email ? `${name} <${email}>` : `${name || email}`
+  const message = {
+    from,
+    to: process.env.EMAIL,
+    subject: `New message from ${from} Contact Form`,
+    text,
+    replyTo: from
+  };
+
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(message, (error, info) =>
+      error ? reject(error) : resolve(info)
+    )
+  })
+}
+
+module.exports = send;
