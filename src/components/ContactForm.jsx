85f4d246b8dac8710f4bec4c6b5df2566c752c4d
David Hersey <david.hersey@holidayextras.com>
2020-05-01 15:33:52 +0100

    How to auth this hmmmmm

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,53 +1,54 @@
 import React from 'react';
-import { reduxForm, Field } from 'redux-form';
-// https://scotch.io/tutorials/managing-form-state-in-react-with-redux-form
-// https://redux-form.com/8.2.2/
+
+// LOOK AT THIS WHOLE PROCESS TO USE GMAIL AUTH AND API TO SEND EMAILS TO ME:
+// https://ciunkos.com/creating-contact-forms-with-nodemailer-and-react
 
 class ContactForm extends React.Component {
+
   render() {
-    const { handleSubmit } = this.props;
     return (
       <div className="col-md-6">
-        <form onSubmit={handleSubmit} className="form">
-          <div className="field form-group">
-            <div className="control">
-              <label className="label">First Name</label>
-              <Field className="input form-control" name="firstName" component="input" type="text" placeholder="First Name"/>
-            </div>
-          </div>
-          <div className="field form-group">
-            <div className="control">
-              <label className="label">Last Name</label>
-              <Field className="input form-control" name="lastName" component="input" type="text" placeholder="Last Name"/>
-            </div>
-          </div>
-          <div className="field form-group">
-            <div className="control">
-              <label className="label">Email</label>
-              <Field className="input form-control" name="email" component="input" type="email" placeholder="Email Address"/>
-            </div>
-          </div>
-          <div className="field form-group">
-            <div className="control">
-              <label className="label">Message</label>
-              <Field className="textarea form-control" name="message" component="textarea" />
-            </div>
-          </div>
-          <div className="field">
-            <div className="control">
-              <button className="button is-link btn btn-primary">Submit</button>
-            </div>
-          </div>
-        </form>
+        <form className="contact-form" method="POST" action="contact">
+    <div className="form-field">
+      <label htmlFor="name">
+        <div className="label-content">Name:</div>
+        <input type="text" name="name" required />
+      </label>
+    </div>
+
+    <div className="form-field">
+      <label htmlFor="email">
+        <div className="label-content">Email:</div>
+        <input type="email" name="email" required />
+      </label>
+    </div>
+
+    <div className="form-field">
+      <label htmlFor="message">
+        <div className="label-content">Message:</div>
+        <textarea className="stretch" name="message" rows="5" required />
+      </label>
+    </div>
+
+    <button type="submit">Send</button>
+
+    <div>
+      { window.location.hash === '#success' &&
+        <div id="success">
+          <p>Your message has been sent!</p>
+        </div>
+      }
+      { window.location.hash === '#error' &&
+        <div id="error">
+          <p>An error occured while submitting the form.</p>
+        </div>
+      }
+    </div>
+  </form>
       </div>
     )
   }
 };
-
-ContactForm = reduxForm({
-  form: 'contact',
-})(ContactForm);
-
 
 export default ContactForm;
 
---
+++ src/server/index.js
@@ -0,0 +1,33 @@
+require('babel-polyfill')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const mailer = require('./mailer')
+
+const app = express()
+
+// NEED TO LOOK AT REDIRECT AFTER SUBMISSIONS
+
+app.set('port', 8000)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('*', (req, res) => {
+  res.send('Server is working. Please post at "/contact" to submit a message.')
+})
+
+app.post('/contact', (req, res) => {
+  const { email = '', name = '', message = '' } = req.body
+
+  mailer({ email, name, text: message }).then(() => {
+    console.log(`Sent the message "${message}" from <${name}> ${email}.`);
+    res.redirect('/#success');
+  }).catch((error) => {
+    console.log(`Failed to send the message "${message}" from <${name}> ${email} with the error ${error && error.message}`);
+    res.redirect('/#error');
+  })
+})
+
+app.listen(8000, () => {
+  console.log(`App listening`);
+})
