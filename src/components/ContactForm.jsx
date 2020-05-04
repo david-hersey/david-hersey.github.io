75e2f966c7e0edc8e85b4a9f48c7d87c58e18702
David Hersey <david.hersey@holidayextras.com>
2020-05-04 11:33:31 +0100

    Remove proxy and netlify up

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,50 +1,42 @@
 import React from 'react';
 
-// LOOK AT THIS WHOLE PROCESS TO USE GMAIL AUTH AND API TO SEND EMAILS TO ME:
-// https://ciunkos.com/creating-contact-forms-with-nodemailer-and-react
-
 class ContactForm extends React.Component {
 
   render() {
+
     return (
       <div className="col-md-6">
-        <form className="contact-form" method="POST" action="contact">
-    <div className="form-field">
-      <label htmlFor="name">
-        <div className="label-content">Name:</div>
-        <input type="text" name="name" required />
-      </label>
-    </div>
-
-    <div className="form-field">
-      <label htmlFor="email">
-        <div className="label-content">Email:</div>
-        <input type="email" name="email" required />
-      </label>
-    </div>
-
-    <div className="form-field">
-      <label htmlFor="message">
-        <div className="label-content">Message:</div>
-        <textarea className="stretch" name="message" rows="5" required />
-      </label>
-    </div>
-
-    <button type="submit">Send</button>
-
-    <div>
-      { window.location.hash === '#success' &&
-        <div id="success">
-          <p>Your message has been sent!</p>
-        </div>
-      }
-      { window.location.hash === '#error' &&
-        <div id="error">
-          <p>An error occured while submitting the form.</p>
-        </div>
-      }
-    </div>
-  </form>
+        <form name="contact" className="form" method="POST" data-netlify="true">
+          <div className="field form-group">
+            <div className="control">
+              <label className="label">First Name</label>
+              <input className="input form-control" name="firstName" component="input" type="text" placeholder="First Name" required />
+            </div>
+          </div>
+          <div className="field form-group">
+            <div className="control">
+              <label className="label">Last Name</label>
+              <input className="input form-control" name="lastName" component="input" type="text" placeholder="Last Name" />
+            </div>
+          </div>
+          <div className="field form-group">
+            <div className="control">
+              <label className="label">Email</label>
+              <input className="input form-control" name="email" component="input" type="email" placeholder="Email Address" required />
+            </div>
+          </div>
+          <div className="field form-group">
+            <div className="control">
+              <label className="label">Message</label>
+              <input className="textarea form-control" name="message" component="textarea" />
+            </div>
+          </div>
+          <div className="field">
+            <div className="control">
+              <button className="button is-link btn btn-primary">Submit</button>
+            </div>
+          </div>
+        </form>
       </div>
     )
   }
