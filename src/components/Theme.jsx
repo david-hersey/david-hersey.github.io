1d6aaec537d84f01925eb33b67a3a2ad521c7f7f
David Hersey <david.hersey@holidayextras.com>
2020-06-29 12:36:57 +0100

    Alter type effect

--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -1,63 +1,8 @@
 import React from 'react'
+import Typing from 'react-typing-animation';
 
 class Theme extends React.Component {
   render () {
-    var TxtRotate = function (el, toRotate, period) {
-      this.toRotate = toRotate
-      this.el = el
-      this.loopNum = 0
-      this.period = parseInt(period, 10) || 2000
-      this.txt = ''
-      this.tick()
-      this.isDeleting = false
-    }
-
-    TxtRotate.prototype.tick = function () {
-      var i = this.loopNum % this.toRotate.length
-      var fullTxt = this.toRotate[i]
-
-      if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1)
-      } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1)
-      }
-
-      this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>'
-
-      var that = this
-      var delta = 300 - Math.random() * 100
-
-      if (this.isDeleting) { delta /= 2 }
-
-      if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period
-        this.isDeleting = true
-      } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false
-        this.loopNum++
-        delta = 500
-      }
-
-      setTimeout(function () {
-        that.tick()
-      }, delta)
-    }
-
-    window.onload = function () {
-      var elements = document.getElementsByClassName('txt-rotate')
-      for (var i = 0; i < elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-rotate')
-        var period = elements[i].getAttribute('data-period')
-        if (toRotate) {
-          new TxtRotate(elements[i], JSON.parse(toRotate), period)
-        }
-      }
-      // INJECT CSS
-      var css = document.createElement('style')
-      css.type = 'text/css'
-      css.innerHTML = '.txt-rotate > .wrap { border-right: 0.08em solid #666 }'
-      document.body.appendChild(css)
-    }
 
     return (
       <div className='container mt-5'>
@@ -67,11 +12,15 @@ class Theme extends React.Component {
         <p>90% of the landing pages are built on a PHP codebase populated by a PHP CMS with the other 10% being migrated to a JS/Node/React based codebase building static S3 served pages populated by cloud based CMS.</p>
         <p>The booking flow solution sits on a JS/Node/React serving S3 pages and incorporates numerous inhouse built microservices and 3rd party integrations</p>
         <h3>Brands worked on:
-          <span
-            class='txt-rotate'
-            data-period='2000'
-            data-rotate='[ " Alton Towers", " Chessington World of Adventures", " Legoland", " Thorpe Park", " Paultons Park", " Warwick Castle" ]'
-          />
+          <Typing speed={100}>
+            <span>Alton Towers</span>
+            <Typing.Backspace count={20} />
+            <Typing.Delay ms={2000} />
+            <span>Chessington World of Adventures</span>
+            <Typing.Backspace count={31} />
+            <Typing.Delay ms={2000} />
+            <span>Legoland</span>
+          </Typing>
         </h3>
       </div>
     )
